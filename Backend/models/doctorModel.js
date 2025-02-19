96c8c53093ebd4fba8e3db329e5247b18febb607
deepak <dkp041297@gmail.com>
2025-02-19 08:44:52 +0530

    user and doctor model is ready

---
+++ Backend/models/doctorModel.js
@@ -0,0 +1,64 @@
+import mongoose from "mongoose";
+const doctorSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    speciality: {
+      type: String,
+      required: true,
+    },
+    degree: {
+      type: String,
+      required: true,
+    },
+    experience: {
+      type: String,
+      required: true,
+    },
+    about: {
+      type: Boolean,
+      available: true,
+    },
+    fees: {
+      type: Number,
+      required: true,
+    },
+    address: {
+      type: Object,
+      required: true,
+    },
+    date: {
+      type: Number,
+      required: true,
+    },
+    slots_booked: {
+      type: Object,
+      default: {},
+    },
+  },
+  { minimize: false }
+);
+
+
+// minimize is false means the default object will be empty so that we can book the slot if we
+// put minimize is true than we can not create doctor data with this object  (  cannot book the slot )
+
+
+const doctorModel = mongoose.models.doctor || mongoose.model('doctor', doctorSchema)
+
+export default  doctorModel
--- Backend/server.js
+++ Backend/server.js
@@ -1,13 +1,25 @@
-// const express = require('express');
-// const app = express();
+import express from "express"
+import cors from "cors"
+import "dotenv/config"
+import connectDB from "./config/mongodb.js"
+import connectCloudinary from "./config/cloudinary.js"
 
-// app.listen(process.env.PORT, () => {
-//   console.log(`Server is running on ${PORT}`);
-// });
+
+const app = express()
+const port =  process.env.PORT ||  4000    
+connectDB() 
+connectCloudinary()
+
+//middleware
+ app.use(express.json) 
+ app.use(cors())
+               
+ //api endpoint
+
+ app.get("/", (req,res)=>{
+    res.send("Hello")
+// console.log("server is running")
+} )
 
 
-import app from "./app";
-
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on ${process.env.PORT}`);
-});
+app.listen(port,()=> console.log("server is running at port 4000"))
