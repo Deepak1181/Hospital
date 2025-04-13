7374cff5c5b784a1d9716e30f0e25c66e50ac932
DEEPAK PRAJAPATI <dkp041297@gmail.com>
2025-04-13 12:13:39 +0530

    vercel

--- Backend/server.js
+++ Backend/server.js
@@ -17,7 +17,12 @@ connectCloudinary()
 
 //middleware
  app.use(express.json()) 
- app.use(cors())
+ app.use(cors({
+    origin: ["*"], 
+    methods: ["GET", "POST", "PUT", "DELETE","UPDATE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
+  }));
+  
                
 //  api endpoint
  app.use("/api/admin",adminRouter)
